7c29e47b1ef4a947c46bee8ef5ad3b1fc4501a3e
irenetam <51868388+irenetam@users.noreply.github.com>
2022-11-13 22:54:24 +0700

    UI Header #7: font, image

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -9,7 +9,6 @@ import {
     faEarthAsia,
     faCircleQuestion,
     faKeyboard,
-    faCloudUpload,
     faUser,
     faCoins,
     faGear,
@@ -25,6 +24,8 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
+import { UploadIcon } from '~/components/icons';
+import Image from '~/components/image';
 
 const cx = classNames.bind(styles);
 
@@ -139,7 +140,7 @@ function Header() {
                         <>
                             <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
                                 <button className={cx('action-btn')}>
-                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                    <UploadIcon />
                                 </button>
                             </Tippy>
                         </>
@@ -153,10 +154,11 @@ function Header() {
                     )}
                     <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                         {currentUser ? (
-                            <img
+                            <Image
                                 className={cx('user-avatar')}
                                 src="https://media.istockphoto.com/id/846730696/photo/portrait-teenager.jpg?b=1&s=170667a&w=0&k=20&c=PNz3dsppr_Q0s_dNI_LaZdoY0oQtH812tvwZ13n-ods="
-                                alt=""
+                                alt="customFallback"
+                                fallback="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-1/299207969_5448054321940699_7547152905224644434_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=jt8fmhrs7DAAX9AWuvv&tn=FwCsqw_zNnW2bv9T&_nc_ht=scontent.fsgn5-6.fna&oh=00_AfCDFjoAJ22AKW2VViP9VJRoYI86g5MyE7cZQkrfC9bQyQ&oe=6376DFAC"
                             />
                         ) : (
                             <button className={cx('more-btn')}>
