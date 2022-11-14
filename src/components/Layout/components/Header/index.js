153117b3eeac9e9f87757e3490fb61f854daadaf
irenetam <51868388+irenetam@users.noreply.github.com>
2022-11-14 11:23:57 +0700

    handle search UI

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,6 @@
-import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faCircleXmark,
-    faSpinner,
-    faMagnifyingGlass,
     faEllipsisVertical,
     faEarthAsia,
     faCircleQuestion,
@@ -15,17 +11,15 @@ import {
     faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
-import HeadLessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 
 import Button from '~/components/Button';
-import { Wrapper as PoperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
-import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
 import { UploadIcon } from '~/components/icons';
 import Image from '~/components/image';
+import Search from '../Search';
 
 const cx = classNames.bind(styles);
 
@@ -61,13 +55,7 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([]);
     const currentUser = true;
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([]);
-        }, 3000);
-    }, []);
 
     const handleMenuChange = (menuItem) => {
         switch (menuItem.type) {
@@ -107,33 +95,7 @@ function Header() {
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok" />
 
-                <HeadLessTippy
-                    interactive={true}
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div className={cx('search-reuslt')} tabIndex="-1" {...attrs}>
-                            <PoperWrapper>
-                                <h4 className={cx('search-title')}>Accounts</h4>
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                                <AccountItem />
-                            </PoperWrapper>
-                        </div>
-                    )}
-                >
-                    <div className={cx('search')}>
-                        <input placeholder="Search accounts and videos" spellCheck={false} />
-                        <button className={cx('clear-btn')}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
-                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                        <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        </button>
-                    </div>
-                </HeadLessTippy>
+                <Search></Search>
 
                 <div className={cx('actions')}>
                     {currentUser ? (
---
+++ src/components/Layout/components/Search/index.js
@@ -0,0 +1,77 @@
+import { useEffect, useState, useRef } from 'react';
+import classNames from 'classnames/bind';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeadLessTippy from '@tippyjs/react/headless';
+import AccountItem from '~/components/AccountItem';
+import styles from './Search.module.scss';
+import { Wrapper as PoperWrapper } from '~/components/Popper';
+import { SearchIcon } from '~/components/icons';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 4]);
+        }, 3000);
+    }, []);
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    return (
+        <HeadLessTippy
+            interactive={true}
+            visible={showResult && searchResult.length > 0}
+            render={(attrs) => (
+                <div className={cx('search-reuslt')} tabIndex="-1" {...attrs}>
+                    <PoperWrapper>
+                        <h4 className={cx('search-title')}>Accounts</h4>
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                    </PoperWrapper>
+                </div>
+            )}
+            onClickOutside={handleHideResult}
+        >
+            <div className={cx('search')}>
+                <input
+                    ref={inputRef}
+                    value={searchValue}
+                    placeholder="Search accounts and videos"
+                    spellCheck={false}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    onFocus={() => setShowResult(true)}
+                />
+                {!!searchValue && (
+                    <button className={cx('clear-btn')} onClick={handleClear}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                )}
+                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                <button className={cx('search-btn')}>
+                    <SearchIcon />
+                </button>
+            </div>
+        </HeadLessTippy>
+    );
+}
+
+export default Search;
