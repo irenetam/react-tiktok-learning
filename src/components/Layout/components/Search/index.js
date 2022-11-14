de26912084fa9aeee44f93ebe418f0a3a1f42356
irenetam <51868388+irenetam@users.noreply.github.com>
2022-11-14 16:16:35 +0700

    use debounced to search after 500ms

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import HeadLessTippy from '@tippyjs/react/headless';
 import AccountItem from '~/components/AccountItem';
 import styles from './Search.module.scss';
 import { Wrapper as PoperWrapper } from '~/components/Popper';
+import { useDebounce } from '~/hook';
 import { SearchIcon } from '~/components/icons';
 
 const cx = classNames.bind(styles);
@@ -16,15 +17,17 @@ function Search() {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced = useDebounce(searchValue, 500);
+
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([]);
             return;
         }
         setLoading(true);
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setSearchResult(res.data);
@@ -33,7 +36,7 @@ function Search() {
             .catch(() => {
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
